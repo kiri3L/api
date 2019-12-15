4e527438734c4e12914d1a09828ed6f70dd19af2
kiri3L <klubimoff@gmai.com>
2019-12-15 22:11:12 +0300

    replace gate to gate_id in methods args, database and JSON replace uid to user_id in methods args, database and JSON add Api-key and config

--- bd_api/models.py
+++ bd_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class gate_uid(models.Model):
     gate_id = models.CharField(max_length=128)
-    uid = models.CharField(max_length=128, null=True)
+    user_id = models.CharField(max_length=128, null=True)
 
 
 class Gate(models.Model):
